3a72dfdb521bc7f3c1ae5b09aed4d301da863256
brennen <brennen.j.sprimont@gmail.com>
2025-05-15 15:21:21 -0500

    exploring game feel options around lobby scene transitions

--- Core/Gameplay/LevelTeleporter.gd
+++ Core/Gameplay/LevelTeleporter.gd
@@ -5,15 +5,21 @@ class_name LevelTeleporter
 @export var label_name: String
 @export var scene_path: String
 
+var started_level_transition: bool = false
+
 func _ready() -> void:
 	$Label3D.text = label_name
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if GameInstance.networking.is_server():
-		if body is Player:
-			var player := body as Player
-			if player.name == str(1):
-				Logger.info("player: %s entered teleporter: label: %s, scene: %s" % [ str(player.name), label_name, scene_path])
-				#GameInstance.load_and_change_scene(scene_path)
-				GameInstance.lobby_load_and_change_scene(scene_path)
-				# TODO: implement game instance way to travel/change scenes with lobby
+		if !started_level_transition:
+			if body is Player:
+				var player := body as Player
+				if player.name == str(1):
+					started_level_transition = true
+					Logger.info("player: %s entered teleporter: label: %s, scene: %s" % [ str(player.name), label_name, scene_path])
+					#GameInstance.load_and_change_scene(scene_path)
+					player.fade_to_black(1.0)
+					await get_tree().create_timer(1.0).timeout
+					GameInstance.lobby_load_and_change_scene(scene_path)
+					# TODO: implement game instance way to travel/change scenes with lobby
